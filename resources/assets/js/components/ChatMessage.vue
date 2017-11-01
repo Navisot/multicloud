3477c41e0c72be9e07c659ab632ac1191add5d49
Navis <nikavr@192.168.1.4>
2017-11-01 12:28:51 +0200

    Initialize Project

---
+++ resources/assets/js/components/ChatMessage.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<p>Hello</p>
+		<small>Agent</small>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>MultiCloud Application</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <div id="app">
+    <h1>Multicloud Application</h1>
+    <example></example>
+    <chat-message></chat-message>
+    </div>
+    <script src="js/app.js" charset="UTF-8"></script>
+</body>
+</html>
